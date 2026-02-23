0a84623304f20360039427b6dec5ba6c5c81956e
Lucas Myers <lucasmmyers1@gmail.com>
2026-02-22 22:02:15 -0600

    day 3

--- oss.cpp
+++ oss.cpp
@@ -3,19 +3,47 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+struct Clock{
+    int seconds;
+    int nanoseconds;
+};
+
+struct PCB{
+    int occupied;
+    pid_t pid;
+    int startSeconds;
+    int startNano;
+    int secEnd;
+    int nanosecEnd;
+
+
+};
+
+Clock simClock;
+simClock.seconds = 0;
+simClock.nanoseconds = 0;
+
+void incrementClock(){
+    
+
+}
+struct PCB processTable[20];
+
 int main (int argCount, char* argValue[]) {
 
 int launched = 0;
 int finished = 0;
-int gonnaLaunch = 0;
-
-int running = 0;
-int maxSims = 0;
+int gonnaLaunch = n;
+int maxSims = n;
 
 int n =5;
 int s = 3;
 float t = 0;
-float interval = 0;
+float i = 0;
+
+for (int x = 0; x < 20; x++){ //processes values in the table
+    processTable[x].occupied = 0;
+}
 
 int option;
 while ((option = getopt(argCount, argValue, "n:s:t:i:")) != -1) {
@@ -38,29 +66,46 @@ while ((option = getopt(argCount, argValue, "n:s:t:i:")) != -1) {
     }
 }
 
-    while (finished < gonnaLaunch){
+    while (launched < n || running > 0){
         int going; //status of child
-        pid_t pid = waitpid(-1, &going,WNOHANG);
+        pid_t pid;
+        while ((pid = waitpid(-1, &going,WNOHANG)) > 0);
         if (pid > 0){
             finished++;
             running--;
-            std::cout << "Child: " << pid <<" terminated\n"
+            std::cout << "Child: " << pid <<" terminated\n";
+
+            //clear table NEEDS TEST
+            //for (int x = 0;vx< 20; x++){
+               // if (processTable[x].pid == pid) {
+                   // processTable[x].occupied = 0;
+                    //break;
+                }
+            }
         }
 
     }
 
 
-    if(launched < gonnaLaunch && running < maxSims){
+    if(launched < n && running < s){
         pid_t child = fork();
         if (child == 0){
-         execl("./worker","./worker",SECONDS HERE, MILLISECONDS HERE, NULL);
+         execl("./worker","./worker",//SECONDS HERE, MILLISECONDS HERE,// NULL);
         perror("fail");
         exit(1);
     } else if (child > 0){
         launched++;
         running++;
+        for (int x= 0; x < 20; x++){
+            if (!processTable[x].occupied){
+                processTable[x].occupied = 1;
+                processTable[x].pid = child;
+                break;
+            }
+        }
     } else {
         perror(" major fail");
     }
 
+}
 }
